f33f1f2dac673abd9649f10e55c9958913ed79a4
Amro-Elsal <amro.elsalahy@dal.ca>
2024-01-15 21:15:19 -0400

    editing about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,11 +20,18 @@ const About = () => {
         <br />
 
         <p className="text-xl">
-        Hello! I am a third-year Computer Science student passionate about coding and debugging. My academic journey 
-        has been fueled by a desire to create innovative solutions that can transform ideas into tangible real-world projects.
-        From the moment I stepped into the field of computer science, I have always been fascinated by the power of code. 
-        The ability to write a piece of code and see it come to life is incredibly satisfying. I enjoy every aspect of the process
-        from designing algorithms to debugging complex issues. The thrill of seeing my code run perfectly, after countless hours of effort and troubleshooting, is truly rewarding.
+        Hello! I am a third-year Computer Science student passionate about coding and debugging. My academic journey has been fueled by a desire to create innovative 
+        solutions that can transform ideas into tangible real-world projects. From the moment I stepped into the field of computer science, I have always been fascinated 
+        by the power of code. The ability to write a piece of code and see it come to life is incredibly satisfying. I enjoy every aspect of the process from designing algorithms
+        to debugging complex issues. The thrill of seeing my code run perfectly, after countless hours of effort and troubleshooting,is truly rewarding.Currently,
+        I am focused on three key areas of growth:
+
+        1- Kotlin for Android Development: I am keen to enhance my skills in Kotlin, specifically in developing Android applications.
+
+        2- C Language: Engaging in a comprehensive study of the C programming language to broaden my understanding of system-level operations.
+        
+        3- Internet Security: I am expanding my knowledge in internet security through getting a Certificate in Communication Technologies and Cyber Security.
+          While the specific details of this area are still being defined, I am eager to understand how to build secure systems and applications.
         </p>
       </div>
     </div>
